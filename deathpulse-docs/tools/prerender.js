fa9fc98cefd55c02b181d87a55551a35e16bb196
b4n9z <b4n9z4tt3r5@gmail.com>
2025-10-23 21:46:16 +0700

    add prerendered test 9

--- deathpulse-docs/tools/prerender.js
+++ deathpulse-docs/tools/prerender.js
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 (async () => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
     for (const route of routes) {
